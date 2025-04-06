36b19a2de00b53e5f765f8ab3b6cda487af216d0
Eduardo <ddzk1111@gmail.com>
2025-04-06 00:08:15 -0300

    1.0

---
+++ imagens.js
@@ -0,0 +1,19 @@
+document.getElementById('loadImages').addEventListener('click', () => {
+    const imagesContainer = document.getElementById('imagesContainer');
+
+    for (let i = 0; i < 3; i++) {
+        fetch('https://api.thedogapi.com/v1/images/search?limit=1') // Utilizei o for pq a API só retorna uma imagem por vez, quando tentei mudar o limit para mais que 1, ela retorna 10 imagens, nao importa o numero
+            .then(response => response.json())
+            .then(data => {
+                const imgElement = document.createElement('img');
+                imgElement.src = data[0].url;
+                imgElement.alt = 'Imagem de um cão';
+                imgElement.className = 'img-thumbnail m-2';
+                imagesContainer.appendChild(imgElement);
+            })
+            .catch(error => {
+                imagesContainer.innerHTML = '<p>Erro ao carregar imagens.</p>';
+                console.error(error);
+            });
+    }
+});
