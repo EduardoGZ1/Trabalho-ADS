fa054210438d3b63ef204d16947ac6ab786ac121
EduardoGZ1 <ddzk1111@gmail.com>
2025-04-09 10:14:13 -0300

    Utilizando async para fazer a requisição

--- imagens.js
+++ imagens.js
@@ -1,20 +1,14 @@
-document.getElementById('loadImages').addEventListener('click', () => {
+(async () => {
     const imagesContainer = document.getElementById('imagesContainer');
     imagesContainer.innerHTML = '';
 
     for (let i = 0; i < 3; i++) {
-        fetch('https://api.thedogapi.com/v1/images/search?limit=1') // Utilizei o for pq a API só retorna uma imagem por vez, quando tentei mudar o limit para mais que 1, ela retorna 10 imagens, nao importa o numero que coloque
-            .then(response => response.json())
-            .then(data => {
-                const imgElement = document.createElement('img');
-                imgElement.src = data[0].url;
-                imgElement.alt = 'Imagem de um cão';
-                imgElement.className = 'img-thumbnail m-2';
-                imagesContainer.appendChild(imgElement);
-            })
-            .catch(error => {
-                imagesContainer.innerHTML = '<p>Erro ao carregar imagens.</p>';
-                console.error(error);
-            });
+        const response = await fetch('https://api.thedogapi.com/v1/images/search?limit=1');
+        const data = await response.json();
+        const imgElement = document.createElement('img');
+        imgElement.src = data[0].url;
+        imgElement.alt = 'Imagem de um cão';
+        imgElement.className = 'img-thumbnail m-2';
+        imagesContainer.appendChild(imgElement);
     }
-});
+})();
--- raças.js
+++ raças.js
@@ -1,13 +1,11 @@
-document.getElementById('loadBreeds').addEventListener('click', () => {
+(async () => {
     const breedsContainer = document.getElementById('breedsContainer');
     breedsContainer.innerHTML = '';
 
-    fetch('https://api.thedogapi.com/v1/breeds?limit=3&page=0')
-        .then(response => response.json())
-        .then(breeds => {
-            breeds.forEach(breed => {
-                const breedInfo = `Nome: ${breed.name} | Temperamento: ${breed.temperament || ''} | Origem: ${breed.origin || ''} | Tempo de vida: ${breed.life_span || ''} | Peso: ${breed.weight.metric || ''} | Altura: ${breed.height.metric || ''}`;
-                breedsContainer.innerHTML += breedInfo + '<br><br>';
-            });
-        });
-});
+    const response = await fetch('https://api.thedogapi.com/v1/breeds?limit=3&page=0');
+    const breeds = await response.json();
+    breeds.forEach(breed => {
+        const breedInfo = `Nome: ${breed.name} | Temperamento: ${breed.temperament || ''} | Origem: ${breed.origin || ''} | Tempo de vida: ${breed.life_span || ''} | Peso: ${breed.weight.metric || ''} | Altura: ${breed.height.metric || ''}`;
+        breedsContainer.innerHTML += breedInfo + '<br><br>';
+    });
+})();
